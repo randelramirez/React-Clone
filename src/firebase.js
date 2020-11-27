739ad79fd7893fa5b221157add905e0070ec4b6a
Randel Ramirez <randelramirez1@gmail.com>
2020-11-27 13:17:55 +0800

    clean up

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC18ajV0pv0gGHoZl1V69ltCOWCKmucmsA',
-  authDomain: 'reactslack-c93e3.firebaseapp.com',
-  databaseURL: 'https://reactslack-c93e3.firebaseio.com',
-  projectId: 'reactslack-c93e3',
-  storageBucket: 'reactslack-c93e3.appspot.com',
-  messagingSenderId: '545566562592',
-  appId: '1:545566562592:web:b8e7de312cecb12ea8df45',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
